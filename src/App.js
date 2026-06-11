92cfacd3661ae3d95498c2698737a03bce680eb0
Het Jain <hetjain2909@gmail.com>
2026-06-11 11:47:07 +0530

    Updated hero section and services

--- src/App.js
+++ src/App.js
@@ -65,13 +65,14 @@ function App() {
           <Navbar />
           <PageWrapper>
             <Routes>
-              <BrowserRouter basename="/BrandUp"></BrowserRouter>
-              <Route path="/" element={<Home />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/crm" element={<CRM />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
+              <BrowserRouter basename="/BrandUp">
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/crm" element={<CRM />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+              </BrowserRouter>
             </Routes>
           </PageWrapper>
           <Footer />
